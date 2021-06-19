49e3b12128371f52713d9eb92329af2bf0d3ee51
MisterMorales <mistermorales1994@gmail.com>
2021-06-19 13:24:21 +0200

    Add DTO

---
+++ src/main/java/com/example/heroes/model/HeroDTO.java
@@ -0,0 +1,15 @@
+package com.example.heroes.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class HeroDTO {
+
+    private Long id;
+
+    private String name;
+}
